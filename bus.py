8de6645fcd5604c42484add842352190288b8ee6
Sara Beldjella <sarabeldjella98@gmail.com>
2025-09-19 16:55:17 +0200

    Añadir clase bus

---
+++ bus.py
@@ -0,0 +1,20 @@
+class Bus:
+    def __init__(self, bCapacidad):
+        self.setCapacidad(bCapacidad)
+        self.__tickets_vendidos = 0
+        self.__tickets_disponibles = bCapacidad
+
+    def setTicketsVendidos(self, isCompra):
+        if isCompra:
+            self.__tickets_vendidos += 1
+        else:
+            self.__tickets_vendidos -= 1
+
+    def setTicketsDisponibles(self, isCompra):
+        if isCompra:
+            self.__tickets_disponibles -= 1
+        else:
+            self.__tickets_disponibles += 1
+
+    def setCapacidad(self, bCapacidad):
+        self.__capacidad = bCapacidad
