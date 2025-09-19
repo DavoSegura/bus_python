a36c34dc4419dccc3e7e482702b33a2cdb1cce8d
Sara Beldjella <sarabeldjella98@gmail.com>
2025-09-19 17:25:00 +0200

    fix billete class

--- billete.py
+++ billete.py
@@ -1,8 +1,11 @@
 from bus import Bus
 
 class Billete:
-    def __init__(self, bus):
-        self.__bus = bus
+    def __init__(self, cliente):
+        self.setCliente(cliente)
 
-    def getBus(self):
-        return self.__bus
+    def getCliente(self):
+        return self.__cliente
+
+    def setCliente(self, cliente):
+        self.__cliente = cliente
