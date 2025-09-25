2bab44b4db030dd766d165f7cb835ec8a26a7106
David Segura <dsegura21121221@gmail.com>
2025-09-25 12:45:58 +0200

    feat: add getters

--- cliente.py
+++ cliente.py
@@ -8,3 +8,9 @@ class Cliente:
 
     def setApellido(self, apellido):
         self.__apellido = apellido
+
+    def getNombre(self):
+        return self.__nombre
+
+    def getApellido(self):
+        return self.__apellido
