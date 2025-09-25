3cadd28e07865ce9c09f3f6e52ca3269ca2fd125
David Segura <dsegura21121221@gmail.com>
2025-09-25 12:52:55 +0200

    feat: add return tickets option

--- options.py
+++ options.py
@@ -34,3 +34,26 @@ def ComprarBilletes(buses):
     else:
         status = f'Error'
     return status
+
+def DevolverBilletes(buses):
+    status= ''
+
+    print("¿Cuál es tu nombre?")
+    nombre = input()
+    print("¿Cuál es tu apellido?")
+    apellido = input()
+
+
+    bus_seleccionado = SeleccionarBus(buses)
+    tickets_vendidos = buses[bus_seleccionado].getTicketsVendidos()
+
+    for billete in tickets_vendidos:
+        cliente = billete.getCliente()
+        if cliente.getNombre() == nombre and cliente.getApellido() == apellido:
+            buses[bus_seleccionado].setTicketsVendidos(False, billete)
+            buses[bus_seleccionado].setTicketsDisponibles(False)
+            status = f'Se devuelto tu billete'
+            break
+    else:
+        status = 'Error'
+    return status
